383ebc6adaf77309f465748feea3ede9bef49899
caochuang <caochuang@caochuangdeMacBook-Pro.local>
2015-04-01 10:12:47 +0800

    Initial Commit

---
+++ TestTabBar/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  TestTabBar
+//
+//  Created by caochuang on 15/4/1.
+//  Copyright (c) 2015年 caochuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ TestTabBar/SecondViewController.m
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.m
+//  TestTabBar
+//
+//  Created by caochuang on 15/4/1.
+//  Copyright (c) 2015年 caochuang. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
